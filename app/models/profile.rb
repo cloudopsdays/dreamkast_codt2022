8e686ab32679d547af211134b655920aff27de48
Kazuto Kusama <jacopen@gmail.com>
2022-04-29 17:33:13 +0900

    Modify registration form

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -19,6 +19,8 @@
 #  updated_at                    :datetime         not null
 #  conference_id                 :integer
 #  company_address_prefecture_id :string(255)
+#  first_name_kana               :string(255)
+#  last_name_kana                :string(255)
 #
 
 class EmailValidator < ActiveModel::EachValidator
@@ -53,11 +55,11 @@ class Profile < ApplicationRecord
   validates :email, presence: true, email: true
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :first_name, presence: true, length: { maximum: 50 }
-  validates :industry_id, presence: true, length: { maximum: 10 }
+  validates :industry_id, length: { maximum: 10 }
   validates :occupation, presence: true, length: { maximum: 50 }
   validates :company_name, presence: true, length: { maximum: 128 }
   validates :company_email, presence: true, email: true
-  validates :company_address, presence: true, length: { maximum: 128 }
+  validates :company_address, length: { maximum: 128 }
   validates :company_tel, presence: true, length: { maximum: 128 }, tel: true
   validates :department, presence: true, length: { maximum: 128 }
   validates :position, presence: true, length: { maximum: 128 }
@@ -69,7 +71,7 @@ class Profile < ApplicationRecord
   end
 
   def self.export(event_id)
-    attr = %w[id email 名 姓 業種 職種 勤務先名/学校名 勤務先メールアドレス 勤務先都道府県 勤務先住所 勤務先電話番号 勤務先部署・所属/学部・学科・学年 勤務先役職]
+    attr = %w[id email 名 姓 メイ セイ 職種 勤務先名/学校名 勤務先メールアドレス 勤務先住所 勤務先電話番号 勤務先部署・所属/学部・学科・学年 勤務先役職]
     CSV.generate do |csv|
       csv << attr
       Profile.where(conference_id: event_id).each do |speaker|
@@ -78,15 +80,15 @@ class Profile < ApplicationRecord
           speaker.email,
           speaker.last_name,
           speaker.first_name,
-          Industry.find(speaker.industry_id).name,
+          speaker.last_name_kana,
+          speaker.first_name_kana,
           speaker.occupation,
           speaker.company_name,
           speaker.company_email,
-          speaker.company_address_prefecture_name,
           speaker.company_address,
           speaker.company_tel,
           speaker.department,
-          speaker.position
+          speaker.position,
         ]
       end
     end
