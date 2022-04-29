d82aa43f874f5ba7c31c7c2d0b1812403185d68a
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2022-04-29 14:06:29 +0900

    add seeds for CODT2022

--- db/fixtures/production/00_seeds.rb
+++ db/fixtures/production/00_seeds.rb
@@ -1,15 +1,40 @@
+Conference.seed(
+  {
+    id: 1,
+    name: 'Cloud Operator Days Tokyo 2022',
+    abbr: 'codt2022',
+    theme: '運用者に光を！〜変革への挑戦〜',
+    copyright: '© Cloud Operator Days Tokyo 2022',
+    privacy_policy: File.read(File.join(Rails.root, 'db/fixtures/production/privacy_policy.md')),
+    coc: File.read(File.join(Rails.root, 'db/fixtures/production/coc.md')),
+    committee_name: '',
+    about: <<'EOS'
+Cloud Operator Days Tokyo は、クラウドの運用者に焦点を当てた技術者向けの新しいテックイベントです。
 
-DUMMY_TEXT = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+クラウドの運用者とは、クラウド基盤（IaaS、PaaS、CaaS、FaaS）を運用しているインフラエンジニアの方だけではなく、クラウド基盤上でアプリケーションを作成し、運用している人も含みます。
 
-Conference.seed()
+オペレーションに関わる技術や悩み、解決策を 発表、共有していくことで運用技術の発展やインフラ、アプリケーション運用に興味がある若手の教育、育成ができるイベントとして発展させていきたいと考えております。
 
-ConferenceDay.seed()
+昨年の第2回は50以上のオンラインセッション、及びワンデーライブイベントを行い、延べ2000名以上の皆様にご参加いただきました。
+https://cloudopsdays.com/archive/2021/
+
+今年の開催はオンラインでのセッション配信、及びオンサイトでのクロージングイベントのハイブリッド開催を計画しております。（状況に応じて変更の可能性があります）
+
+セッションは全て事前撮影した動画の放映といたします。１セッションあたりの時間は、20分を予定しています。
+EOS
+  }
+)
+
+ConferenceDay.seed(
+  {id: 1, date: "2022-07-25", start_time: "13:00", end_time: "19:00", conference_id: 1, internal: false},
+  {id: 2, date: "2022-05-31", start_time: "19:00", end_time: "21:00", conference_id: 1, internal: true} #Pre event
+)
 
 Industry.seed()
 
 FormItem.seed()
 
-Track.seed
+Track.seed()
 
 TalkCategory.seed()
 
