bd91d7701d78970480d9615cafd80a1a3d9a8346
Kazuto Kusama <jacopen@gmail.com>
2022-05-13 00:01:05 +0900

    Fix links and texts

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -34,8 +34,9 @@ class ProfilesController < ApplicationController
       Agreement.create!(profile_id: @profile.id, form_item_id: 3, value: 1) if agreement_params['require_posting']
       Agreement.create!(profile_id: @profile.id, form_item_id: 4, value: 1) if agreement_params['agree_ms']
       Agreement.create!(profile_id: @profile.id, form_item_id: 5, value: 1) if agreement_params['agree_ms_cndo2021']
-      ProfileMailer.registered(@profile, @conference).deliver_later
-      redirect_to("/#{event_name}/timetables")
+      # ProfileMailer.registered(@profile, @conference).deliver_later  # FIXME: Temporary disabled. Need to fix fifo-worker and SES settings.
+      redirect_to("/#{event_name}/dashboard")
+      # redirect_to("/#{event_name}/timetables") # TODO: Need to uncomment after pre-event
     else
       respond_to do |format|
         format.html { render(:new) }
