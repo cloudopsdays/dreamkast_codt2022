085c4621ddfd894a0649058ab56c8c92255a599b
Kazuto Kusama <jacopen@gmail.com>
2022-05-13 00:48:22 +0900

    fix spec

--- spec/requests/tracks_spec.rb
+++ spec/requests/tracks_spec.rb
@@ -27,43 +27,6 @@ RSpec.describe(TracksController, type: :request) do
           expect(response.body).to(include('<a class="dropdown-item" href="/codt2022/admin">管理画面</a>'))
         end
 
-        context 'user is speaker' do
-          let!(:alice) { create(:speaker_alice) }
-          it 'show speaker' do
-            subject
-            expect(response).to(be_successful)
-            expect(response.body).to(include('<h4>Alice様へのお知らせ</h4>'))
-          end
-
-          context 'wnen announcement is not published' do
-            before do
-              create(:speaker_announcement, speakers: [alice])
-            end
-            it 'not exists speaker_announcements' do
-              subject
-              expect(response).to(be_successful)
-              expect(response.body).to(include('<h4>Alice様へのお知らせ</h4>'))
-              expect(response.body).not_to(include('<p>test_announcement</p>'))
-            end
-          end
-
-          context 'when announcement is published' do
-            before do
-              create(:speaker_announcement, :published, speakers: [alice])
-              create(:speaker_announcement, :speaker_mike, speakers: [create(:speaker_mike)])
-            end
-
-            it "show only alice's announcements" do
-              subject
-              expect(response).to(be_successful)
-              expect(response.body).to(include('<h4>Alice様へのお知らせ</h4>'))
-              expect(response.body).to(include('<p>test announcement for alice</p>'))
-              expect(response.body).not_to(include('<p>test announcement for mike</p>'))
-              expect(SpeakerAnnouncement.where(publish: true).size).to(eq(2))
-            end
-          end
-        end
-
         context 'user is not speaker' do
           it 'not show speaker_announcements' do
             subject
