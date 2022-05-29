31b350561c383bccb7902a23e097cbe38e2212f0
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2022-05-29 17:58:13 +0900

    rubocop

--- app/controllers/admin/talks_controller.rb
+++ app/controllers/admin/talks_controller.rb
@@ -52,7 +52,7 @@ class Admin::TalksController < ApplicationController
     )
 
     flash[:notice] = "OnAirに切り替えました: #{talk.start_to_end} #{talk.speaker_names.join(',')} #{talk.title}"
-    redirect_to admin_tracks_path
+    redirect_to(admin_tracks_path)
   end
 
   def stop_on_air
@@ -77,7 +77,7 @@ class Admin::TalksController < ApplicationController
     )
 
     flash[:notice] = "Waiting に切り替えました: #{talk.start_to_end} #{talk.speaker_names.join(',')} #{talk.title}"
-    redirect_to admin_tracks_path
+    redirect_to(admin_tracks_path)
   end
 
   def start_recording
