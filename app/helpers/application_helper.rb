116bf992cf89f60f0c4d7a80958b1d9d4eb9ec62
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2022-06-01 19:54:42 +0900

    support multiple vimeo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -69,6 +69,15 @@ module ApplicationHelper
     link_to(name, '#', class: 'add_key_image_fields ' + args[:class], data: { id: id, fields: fields.gsub("\n", '') }, style: args[:style])
   end
 
+  def link_to_add_vimeo_fields(name, f, association, **args)
+    new_object = f.object.to_model.class.reflect_on_association(association).klass.new
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + '_fields', f: builder)
+    end
+    link_to(name, '#', class: 'add_vimeo_fields ' + args[:class], data: { id: id, fields: fields.gsub("\n", '') }, style: args[:style])
+  end
+
   def link_to_add_link_fields(name, f, association, **args)
     new_object = f.object.to_model.class.reflect_on_association(association).klass.new
     id = new_object.object_id
