18bb07cbde53f4a4f1b589eb984b668a81647869
Kazuto Kusama <jacopen@gmail.com>
2022-06-26 02:49:29 +0900

    Change interval from 120sec to 60sec

--- app/javascript/packs/talks.js
+++ app/javascript/packs/talks.js
@@ -1,18 +1,18 @@
-$(function(){
+$(function() {
     clearInterval(window.timer);
     console.log("timer activated");
 
-    window.timer = setInterval(function(){
+    window.timer = setInterval(function() {
         console.log("sending logs...");
         tracker.track("watch_video", {
             track_name: "archive",
             talk_id: window.talk_id,
             talk_name: window.talk_name
         });
-    }, 120 * 1000);
+    }, 60 * 1000);
 })
 
-window.tableFilterStripHtml = function (value) {
+window.tableFilterStripHtml = function(value) {
     return value.replace(/<[^>]+>/g, '').trim();
 }
 
@@ -20,6 +20,6 @@ $(document).on('turbolinks:load', function() {
     $('[data-toggle="table"]').bootstrapTable();
 });
 
-$(window).resize(function () {
+$(window).resize(function() {
     $('.talks-frame').bootstrapTable('resetView')
 })
